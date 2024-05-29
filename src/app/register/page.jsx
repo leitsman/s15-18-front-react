ddc60a5e4bc5b54627ab0f3701652b1a750aa25d
nacho <jose-imhoff@hotmail.com>
2024-05-29 19:08:41 -0300

    Prueba pull

---
+++ src/app/register/page.jsx
@@ -0,0 +1,7 @@
+export const page =()=>{
+    <main className='w-screen h-screen border-4 border-black border-dashed flex flex-col justify-center items-center'>
+        <div>
+            <h1 className="text-red-900 font-bold">Registro</h1>
+        </div>
+    </main>
+}
