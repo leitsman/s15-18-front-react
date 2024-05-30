b6db1dc8f0e8918818fd3fd825a98d27a7f4afca
nacho <jose-imhoff@hotmail.com>
2024-05-29 22:29:41 -0300

    Creacion boceto pagina de registro

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,7 +1,20 @@
-export const page =()=>{
-    <main className='w-screen h-screen border-4 border-black border-dashed flex flex-col justify-center items-center'>
-        <div>
-            <h1 className="text-red-900 font-bold">Registro</h1>
-        </div>
+import Image from "next/image";
+import React from "react";
+import RegisterForm from "../../components/features/register/registerform"
+const page = () => {
+  return (
+    <main className="w-screen h-screen flex flex-col items-center">
+      <div className="w-3/5 flex flex-col justify-center items-center gap-10">
+        <div className="w-1/2 bg-gray-300">
+          <Image src="" width={168.95} height={168.95} alt="imagen" />
+        </div>    
+        <h1 className="text-center text-6xl">Crea tu cuenta</h1>
+        
+        <a className="flex items-center space-x-2 justify-center w-64 text-black bg-gray-200 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-2xl text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 font-bold" ><span>CONTINUAR CON GOOGLE</span></a>
+      </div>
+      
+      <RegisterForm />
     </main>
-}
+  );
+};
+export default page;
