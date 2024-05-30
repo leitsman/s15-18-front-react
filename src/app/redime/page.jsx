3e3b014ea9078d710348941d216e56080d336371
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2024-05-30 11:34:35 -0500

    feat: add# redime-page

--- src/app/redime/page.jsx
+++ src/app/redime/page.jsx
@@ -36,7 +36,7 @@ const page = () => {
 
   return (
     <>
-      <main className="w-screen h-[calc(100vh-96px)] flex flex-col gap-10 py-5 px-10 ">
+      <main className="w-screen h-[calc(100vh-96px)] flex flex-col gap-10 py-5 px-8 ">
         {/* HEADER */}
         <div className="w-full flex p-2 justify-between items-center h-16 ">
           <div className="flex justify-center items-center gap-5 ">
