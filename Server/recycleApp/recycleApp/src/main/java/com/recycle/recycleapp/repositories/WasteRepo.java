f96ce67376854f8e5546b3260fd3cadd92106eac
Leonel Amaya <omar.mejia2099@gmail.com>
2024-06-01 10:55:51 -0500

    fix: reorganize class imports Waste and RecCntr

--- Server/recycleApp/recycleApp/src/main/java/com/recycle/recycleapp/repositories/WasteRepo.java
+++ Server/recycleApp/recycleApp/src/main/java/com/recycle/recycleapp/repositories/WasteRepo.java
@@ -1,10 +1,8 @@
-package com.recycle.recycleapp.entity;
-
+package com.recycle.recycleapp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WasteRepo extends JpaRepository<Waste,Long> {
-
+public interface WasteRepo extends JpaRepository<com.recycle.recycleapp.entity.Waste,Long> {
 }
