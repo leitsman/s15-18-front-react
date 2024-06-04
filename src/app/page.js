3e67d21289b5f85216053f367445a07d84680281
nacho <jose-imhoff@hotmail.com>
2024-06-03 21:46:37 -0300

    Cambios y agregado de la funcion de ocultar contraseñas en el componente

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
 import Welcome from '@/components/mdx/Welcome.mdx'
 import { Textfield } from '@/components/ui/textfield';
 import "../styles/globals.css";
-import CustomButtonIcon from '@/components/ui/custombuttonicon';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="font-semibold">
         Hola team: s15-18-t-java-react
       </h1>
-      <Textfield className="w-72 h-9 border-b-2 border-green-300 placeholder-custom focus:outline-none" placeholder="Label" type="password" />
+      <Textfield className="w-72 h-9 border-b-2 border-green-300 placeholder-custom focus:outline-none" placeholder="Label" type="text" />
       <Welcome />
     </main>
   );
