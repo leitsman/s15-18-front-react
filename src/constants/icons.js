97fff3e12d1e446e7e891861659d6c4786d7cc18
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2024-06-05 11:20:39 -0500

    created themes

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,10 +1,11 @@
-import { FiBell, FiMenu, FiChevronRight, } from "react-icons/fi";
-import { MdOutlineEco, MdRedeem, MdRecycling, MdNewspaper  } from "react-icons/md";
+import { FiBell, FiMenu, FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { MdOutlineEco, MdRedeem, MdRecycling, MdNewspaper } from "react-icons/md";
 
 export const iconNames = {
   bell: FiBell,
   menu: FiMenu,
   rightArrow: FiChevronRight,
+  leftArrow: FiChevronLeft,
   eco: MdOutlineEco,
   redeem: MdRedeem,
   recycle: MdRecycling,
