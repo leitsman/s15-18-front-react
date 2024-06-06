b58cd00024899727f4fc13bc8d5abd3ca22190ad
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2024-06-06 10:33:02 -0500

    feat: authorization, middlewares

--- src/components/ui/breadCrumbs.jsx
+++ src/components/ui/breadCrumbs.jsx
@@ -1,18 +1,27 @@
+"use client";
+
 import React from "react";
 import { Stack } from "../layout/stack";
 import { Icon } from "./icon";
 import { Text } from "./text";
 import { Button } from "./button";
+import { useRouter } from "next/navigation";
 
-export const BreadCrumbs = ({ currentRoute }) => {
+export const BreadCrumbs = ({ currentRoute, children }) => {
+  const router = useRouter();
+
   return (
     <Stack variant="inline" distribution="start" alignment="center">
-      <Button className="p-0 w-fit h-fit bg-transparent hover:bg-transparent text-foreground">
-        <Icon iconName="leftArrow" size="md" className="" />
+      <Button
+        className="p-0 w-fit h-fit bg-transparent hover:bg-transparent text-foreground"
+        onClick={() => router.back()}
+      >
+        <Icon iconName="leftArrow" size="lg" className="" />
       </Button>
-      <Text variant="subtitle" className="w-full text-center">
+      <Text variant="subtitle" size="lg" className="w-full text-center ">
         {currentRoute}
       </Text>
+      {children}
     </Stack>
   );
 };
