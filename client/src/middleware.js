80811a6de0a644e5481e676d8053173c78fa99ff
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2024-06-07 14:14:07 -0500

    integration: client folder updated

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -2,6 +2,7 @@ import AuthMiddleware from "./middlewares/auth.middleware";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
+  return
   return AuthMiddleware(request);
 }
 
