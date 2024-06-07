07d1a01d7f597503402adc61e5f296b25f1c386e
Valerie Ramos <valerie22@live.com>
2024-06-07 15:08:40 -0500

    updating application.properties

--- Server/recycleApp/recycleApp/src/main/java/com/recycle/recycleapp/config/OpenApiConfig.java
+++ Server/recycleApp/recycleApp/src/main/java/com/recycle/recycleapp/config/OpenApiConfig.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -50,5 +51,6 @@ import io.swagger.v3.oas.annotations.servers.Server;
         bearerFormat = "JWT",
         in = SecuritySchemeIn.HEADER
 )
+@Configuration
 public class OpenApiConfig {
 }
