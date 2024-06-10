e97469be2d61fbe4f0301b1c99b580941db23661
Valerie Ramos <valerie22@live.com>
2024-06-10 16:10:17 -0500

    testing

--- Server/recycleApp/recycleApp/src/main/java/com/recycle/recycleapp/config/OpenApiConfig.java
+++ Server/recycleApp/recycleApp/src/main/java/com/recycle/recycleapp/config/OpenApiConfig.java
@@ -11,38 +11,38 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(
-                contact = @Contact(
-                        name = "Recycle-app",
-                        email = "contact@recycle.com",
-                        url = "https://recycle.com/course"
-                ),
-                description = "OpenApi documentation for Spring Security",
-                title = "OpenApi specification - recycle-app",
-                version = "1.0",
-                license = @License(
-                        name = "Licence name",
-                        url = "https://some-url.com"
-                ),
-                termsOfService = "Terms of service"
-        ),
-        servers = {
-                @Server(
-                        description = "Local ENV",
-                        url = "http://localhost:8088/api/v1"
-                ),
-                @Server(
-                        description = "PROD ENV",
-                        url = "https://s15-18-t-java-react.onrender.com"
-                )
-        },
-        security = {
-                @SecurityRequirement(
-                        name = "bearerAuth"
-                )
-        }
-)
+//@OpenAPIDefinition(
+//        info = @Info(
+//                contact = @Contact(
+//                        name = "Recycle-app",
+//                        email = "contact@recycle.com",
+//                        url = "https://recycle.com/course"
+//                ),
+//                description = "OpenApi documentation for Spring Security",
+//                title = "OpenApi specification - recycle-app",
+//                version = "1.0",
+//                license = @License(
+//                        name = "Licence name",
+//                        url = "https://some-url.com"
+//                ),
+//                termsOfService = "Terms of service"
+//        ),
+//        servers = {
+//                @Server(
+//                        description = "Local ENV",
+//                        url = "http://localhost:8080/api/v1"
+//                ),
+//                @Server(
+//                        description = "PROD ENV",
+//                        url = "https://s15-18-t-java-react.onrender.com"
+//                )
+//        },
+//        security = {
+//                @SecurityRequirement(
+//                        name = "bearerAuth"
+//                )
+//        }
+//)
 @SecurityScheme(
         name = "bearerAuth",
         description = "JWT auth description",
@@ -53,4 +53,5 @@ import org.springframework.context.annotation.Configuration;
 )
 @Configuration
 public class OpenApiConfig {
+
 }
