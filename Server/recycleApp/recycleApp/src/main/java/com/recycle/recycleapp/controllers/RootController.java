60761c312da9f35d425419fffb22bf422407e101
Valerie Ramos <valerie22@live.com>
2024-06-10 15:24:15 -0500

    new controller

---
+++ Server/recycleApp/recycleApp/src/main/java/com/recycle/recycleapp/controllers/RootController.java
@@ -0,0 +1,16 @@
+package com.recycle.recycleapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+
+    @GetMapping
+    public String root() {
+        return "Welcome to the RecycleApp API!";
+    }
+}
